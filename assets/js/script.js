24fd2d7beb8fabceb6c4f48743ea267b816f0063
dev-rashidul <rashidul.contact@gmail.com>
2023-01-29 00:29:48 +0600

    Homepage done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,104 @@ window.addEventListener("scroll", function () {
 
 // Hero Slider JavaScript
 
-$('.hero-slider').slick({
+$(".hero-slider").slick({
   arrows: true,
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 4000,
-  fade:true,
+  fade: true,
   pauseOnHover: false,
   //infinite: true,
   Speed: 800,
-  prevArrow: '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
-  nextArrow: '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
+  prevArrow:
+    '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
+  nextArrow:
+    '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
+});
+
+// Sponsor Slider JavaScript
+
+$(".sponsor-slider").slick({
+  arrows: false,
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+  prevArrow:
+    '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
+  nextArrow:
+    '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
+});
+
+// Home Event Slider JavaScript
+
+$(".home-event-slider").slick({
+  arrows: false,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  fade: true,
+  pauseOnHover: false,
+  //infinite: true,
+  Speed: 800,
+  prevArrow:
+    '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
+  nextArrow:
+    '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
+});
+
+// Mamanets Players Slider JavaScript
+
+$(".mamanets-players-slider").slick({
+  arrows: true,
+  dots: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+  prevArrow:
+    '<span class="slick-prev"><i class="fa-solid fa-chevron-left"></i></span>',
+  nextArrow:
+    '<span class="slick-next"><i class="fa-solid fa-chevron-right"></i></span>',
 });
